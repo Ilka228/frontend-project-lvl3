f90f25eaacd66e2f70309ccd28061c0e9562c033
IlyaSolonovich <ilyasolonovich20@gmail.com>
2022-08-18 14:02:22 +0200

    fix: normilize code with eslint

--- src/index.js
+++ src/index.js
@@ -2,43 +2,40 @@ import 'bootstrap';
 import './styles/main.css';
 import { string } from 'yup';
 
-
 const state = {
-    rssForm: {
-        isValid: true
-    }
-}
+  rssForm: {
+    isValid: true,
+  },
+};
 
 const validation = (element) => {
-
-   const formIsValid = (element) => {
-        if(!state.rssForm.isValid) {
-            element.classList.add('invalid');
-        } else {
-          element.classList.remove('invalid');
-        }
+  const formIsValid = (element) => {
+    if (!state.rssForm.isValid) {
+      element.classList.add('invalid');
+    } else {
+      element.classList.remove('invalid');
     }
+  };
 
-    const validate = (input) => {
-        let schema = string().matches(/.rss$/).url().nullable();
-        schema.isValid(input)
-            .then((cb) => state.rssForm.isValid = cb)
-            .then(() => console.log(state.rssForm.isValid))
-            .then(() => formIsValid(element)); 
-    }
+  const validate = (input) => {
+    const schema = string().matches(/.rss$/).url().nullable();
+    schema.isValid(input)
+      .then((cb) => state.rssForm.isValid = cb)
+      .then(() => console.log(state.rssForm.isValid))
+      .then(() => formIsValid(element));
+  };
 
-    validate(element.value);
-    
-}
+  validate(element.value);
+};
 
 const form = document.createElement('form');
 
-form.innerHTML = `<form class="rss-form"><input type="text" class="rss-form_input" name="input"><input class="rss-form_submit" type="submit" value="Save"></form>`;
+form.innerHTML = '<form class="rss-form"><input type="text" class="rss-form_input" name="input"><input class="rss-form_submit" type="submit" value="Save"></form>';
 document.body.appendChild(form);
 
 const rssForm = document.querySelector('form');
 const rssFormInput = rssForm.elements.input;
-rssForm.addEventListener('submit', function(e){
-    e.preventDefault();
-    validation(rssFormInput);
+rssForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  validation(rssFormInput);
 });
