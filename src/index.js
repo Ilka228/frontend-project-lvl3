7d375042ab727cb3878bf6dbe47de6dc6eeb30ba
IlyaSolonovich <ilyasolonovich20@gmail.com>
2022-08-28 11:53:54 +0200

    feat: add parser

--- src/index.js
+++ src/index.js
@@ -1,16 +1,54 @@
 import 'bootstrap';
 import './styles/main.css';
 import { string } from 'yup';
-import parse from './js/hello.js';
+import i18next from "i18next";
+import yup , { setLocale } from 'yup';
+
+
 
 const axios = require('axios').default;
 
 const state = {
   rssForm: {
-    isValid: true,
+    isValid: null,
     forError: null,
   },
+  elements: [],
+  streams: [],
 };
+
+
+
+i18next.init({
+  lng: 'ru', 
+  debug: false,
+  resources: {
+    ru: {
+      translation: {
+        pageTexContent: {
+
+        },
+        errors:{
+          "not_a_URL": "Ссылка должна быть валидным URL"
+        }
+      }
+    }
+  }
+});
+
+setLocale({
+  string: {
+    url: () => ({ key: 'not_a_URL' }),
+    nullable: () => ({ key: 'not_a_URL' })
+  },
+});
+
+// try {
+//   await schema.validate({ name: 'jimmy', age: 11 });
+// } catch (err) {
+//   console.log(err.errors.map((err) => i18next.t(err.key)));
+// }
+
 
 const validation = (element) => {
   
@@ -22,41 +60,41 @@ const validation = (element) => {
     }
   };
 
-  const validate = (input) => {
-    const schema = string().matches(/rss/).url().nullable();
+  const valid = (input) => {
+    const schema = string().url().defined();
     schema.isValid(input)
+      //.catch((err) => console.log(i18next.t(err.key)))
       .then((cb) => { state.rssForm.isValid = cb; })
       .then(() => console.log(state.rssForm.isValid))
       .then(() => formIsValid(element));
   };
 
-  validate(element.value);
+  return valid(element.value);
 };
 
-const formSection = document.createElement('header');
-formSection.classList.add('header');
-const form = document.createElement('form');
-form.innerHTML = `
-    <form class="rss-form">
-      <div class="form-group"> 
-        <input type="text" class="rss-form_input form-control" name="input" placeholder="RSS link" autocomplete="off">
-        <button type="submit" class="btn btn-primary rss-form_submit">Добавить</button>
-      </div>
-      <small id="emailHelp" class="form-text text-muted">Example: https://ru.hexlet.io/lessons.rss</small>
-      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-    </form>`;
-
-document.body.appendChild(formSection);
-const section = document.querySelector('.header');
-section.appendChild(form);
-
-
+const main = document.createElement('main');
+main.innerHTML = `
+ 
+    <section class="rss-section">
+      <div class="wrapper">
+        <h1>RSS агрегатор</h1>
+        <p class="description">Начните читать RSS сегодня! Это легко, это красиво.</p>
+        <form class="rss-form">
+          <div class="form-group"> 
+            <input type="text" class="rss-form_input form-control" name="input" placeholder="RSS link" autocomplete="off">
+            <button type="submit" class="btn btn-primary rss-form_submit">Добавить</button>
+          </div>
+        </form>
+        <p class="rss_example">Пример: https://ru.hexlet.io/lessons.rss</p>
+        <p></p>
+      </wrapper>
+    </section>
+  
+`
+document.body.appendChild(main);
 const rssForm = document.querySelector('form');
 const rssFormInput = rssForm.elements.input;
-rssForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  validation(rssFormInput);
-});
+
   
 
   // axios({
@@ -75,13 +113,71 @@ rssForm.addEventListener('submit', (e) => {
   // }).then(function (response) {
   //   console.log(response.data);
   // });
- 
-  fetch(`https://allorigins.hexlet.app/get?&url=${encodeURIComponent('http://rss.dw.de/xml/rss-ru-news')}`)
+  const parse = (doc, url) => {
+    const parsedStream = new DOMParser().parseFromString(doc, "application/xml");
+    console.log(parsedStream);
+    const stream = parsedStream.querySelector('channel');
+    const addStream = () => {
+      const isStreamInState = state.streams.some((stream) => stream.link === url);
+      if(isStreamInState) return;
+      else {
+        state.streams.push({
+          title: stream.querySelector('title').innerHTML,
+          description: stream.querySelector('description').innerHTML,
+          link: url,
+        });
+      }
+    }
+    addStream();
+    const items = parsedStream.querySelectorAll('item');
+    console.log(items);
+    
+    items.forEach((item) => {
+      const isElementInState = state.elements.some((element) => element.link === item.querySelector('link').innerHTML);
+      if(isElementInState) return;
+      else {
+        state.elements.push({
+          link: item.querySelector('link').innerHTML, 
+          description: item.querySelector('description').innerHTML,
+          title: item.querySelector('title').innerHTML,
+        })
+      }
+    });
+    console.log(state.elements);
+    console.log(state.streams);
+}
+// const url = 'https://ru.hexlet.io/lessons.rss';
+
+//   fetch(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`)
+//   .then(response => {
+//     if (response.ok) return response.json();
+//     throw new Error('Network response was not ok.');
+//   })
+//   .then(data => parse(data.contents, url));
+  // fetch(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`)
+  // .then(response => {
+  //   if (response.ok) return response.json();
+  //   throw new Error('Network response was not ok.');
+  // })
+  // .then(data => parse(data.contents, url));
+
+const query = (url) => {
+  fetch(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`)
   .then(response => {
     if (response.ok) return response.json();
     throw new Error('Network response was not ok.');
   })
-  .then(data => parse(data.contents));
-
-
+  .then(data => parse(data.contents, url));
+}
+// console.log(rssForm.elements.input.value);
+// query(rssFormInput);
   // var doc = parser.parseFromString(data.contents, "application/xml");
+  rssForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    validation(rssFormInput);
+    if(!state.rssForm.isValid) return;
+    else{ 
+      query(rssFormInput.value);
+    }
+    
+  });
